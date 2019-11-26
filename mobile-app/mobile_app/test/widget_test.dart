9126bef7a925571c09a546adb3adf9ad760db1b9
psidae <akson.rill@gmail.com>
2019-11-26 17:31:35 +0200

    update test

--- mobile-app/mobile_app/test/widget_test.dart
+++ mobile-app/mobile_app/test/widget_test.dart
@@ -11,38 +11,7 @@ import 'package:mockito/mockito.dart';
 
 import 'package:mobile_app/main.dart';
 
-class MockNavigatorObserver extends NavigatorObserver {
-  OnObservation onPushed;
-  OnObservation onPopped;
-  OnObservation onRemoved;
-  OnObservation onReplaced;
-
-  @override
-  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
-    if (onPushed != null) {
-      onPushed(route, previousRoute);
-    }
-  }
-
-  @override
-  void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
-    if (onPopped != null) {
-      onPopped(route, previousRoute);
-    }
-  }
-
-  @override
-  void didRemove(Route<dynamic> route, Route<dynamic> previousRoute) {
-    if (onRemoved != null)
-      onRemoved(route, previousRoute);
-  }
-
-  @override
-  void didReplace({ Route<dynamic> oldRoute, Route<dynamic> newRoute }) {
-    if (onReplaced != null)
-      onReplaced(newRoute, oldRoute);
-  }
-}
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
   testWidgets('Button is present and triggers navigation after tapped', (WidgetTester tester) async {
@@ -61,10 +30,6 @@ void main() {
 
     verify(mockObserver.didPush(typed(any), typed(any)));
 
-    expect(find.byType(MaterialPageRoute), findsOneWidget)
-
-    // Verify that our counter has incremented.
-    // expect(find.text('Item 0'), findsNothing);
-    // expect(find.text('Item 1'), findsOneWidget);
+    expect(find.byType(Navigator), findsOneWidget);
   });
 }
