dd12c28236695ad2ad5725eee2d38a1dd53971f0
Spok <game1life1@gmail.com>
2019-11-26 22:06:22 +0200

    setup redux, redux saga

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import store, { action } from './store';
+
+import { Provider } from 'react-redux';
+
+import './App.sass';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          <button onClick={() => action('TEST_ASYNC')}>btn</button>
+        </header>
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
